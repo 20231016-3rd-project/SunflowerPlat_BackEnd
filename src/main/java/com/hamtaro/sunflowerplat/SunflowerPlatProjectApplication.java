f49fd93dd1dcca080683a226bc51ffd3a95bc493
Taeyeon-0314 <lty0314@gmail.com>
2023-10-20 13:39:27 +0900

    init: 프로젝트 초기 생성

---
+++ src/main/java/com/hamtaro/sunflowerplat/SunflowerPlatProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hamtaro.sunflowerplat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunflowerPlatProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunflowerPlatProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamtaro/sunflowerplat/SunflowerPlatProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamtaro.sunflowerplat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunflowerPlatProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
