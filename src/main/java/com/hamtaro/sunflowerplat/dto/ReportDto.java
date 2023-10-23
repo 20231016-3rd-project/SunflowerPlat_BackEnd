5c40f2df022c950ad6db2cc400f40b6349d0c0f2
sojinb <thwls3791@naver.com>
2023-10-23 16:59:12 +0900

    feat: Dto 생성

---
+++ src/main/java/com/hamtaro/sunflowerplat/dto/ReportDto.java
@@ -0,0 +1,20 @@
+package com.hamtaro.sunflowerplat.dto;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReportDto {
+
+    private String reportCategory;
+
+    private String reportContent;
+
+    private LocalDate reportAt;
+}
