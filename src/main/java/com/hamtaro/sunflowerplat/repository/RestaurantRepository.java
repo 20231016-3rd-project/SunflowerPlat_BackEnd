03ae8ab66a2a75cd4d3f5748411d6a993e40302e
sojinb <thwls3791@naver.com>
2023-10-23 17:34:27 +0900

    feat: repository 추가

--- src/main/java/com/hamtaro/sunflowerplat/repository/RestaurantRepository.java
+++ src/main/java/com/hamtaro/sunflowerplat/repository/RestaurantRepository.java
@@ -3,5 +3,8 @@ package com.hamtaro.sunflowerplat.repository;
 import com.hamtaro.sunflowerplat.entity.restaurant.RestaurantEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RestaurantRepository extends JpaRepository<RestaurantEntity,Long> {
+    Optional<RestaurantEntity> findByRestaurantId(Long useId);
 }
