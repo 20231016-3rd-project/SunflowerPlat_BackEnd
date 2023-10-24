4c63912c26ebfd37bd13c400d3fb88918699f4c6
Taeyeon-0314 <lty0314@gmail.com>
2023-10-24 12:12:27 +0900

    feat : github-actions cicd 구축

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 cd /home/ec2-user/app
-DOCKER_APP_NAME=spring
-# 실행중인 blue가 있는지
-EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep running)
-# green이 실행중이면 blue up
+
+export DOCKER_APP_NAME= sunflowerplate DOCKER_APP_NAME=back-zero-downtime IMAGE_TAG=latest
+
+EXIST_BLUE=$(docker compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+
 if [ -z "$EXIST_BLUE" ]; then
-	echo "blue up"
-	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
-	sleep 30
-	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
-	docker image prune -af # 사용하지 않는 이미지 삭제
+    echo "blueis is not exist. so make blue container"
+    echo "blue up"
+    docker compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d
+    BEFORE_COMPOSE_COLOR="green"
+    AFTER_COMPOSE_COLOR="blue"
+    echo "end"
 else
-	echo "green up"
-	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
-	sleep 30
-	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
-	docker image prune -af
+    echo "blue is exist. so make green container"
+    echo "green up"
+    docker compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d
+    BEFORE_COMPOSE_COLOR="blue"
+    AFTER_COMPOSE_COLOR="green"
+fi
+
+sleep 20
+
+EXIST_AFTER=$(docker compose -p ${DOCKER_APP_NAME}-${AFTER_COMPOSE_COLOR} -f docker-compose.${AFTER_COMPOSE_COLOR}.yml ps | grep Up)
+if [ -n "$EXIST_AFTER" ]; then
+
+    docker compose -p ${DOCKER_APP_NAME}-${BEFORE_COMPOSE_COLOR} -f docker-compose.${BEFORE_COMPOSE_COLOR}.yml down
+    echo "$BEFORE_COMPOSE_COLOR down"
 fi
