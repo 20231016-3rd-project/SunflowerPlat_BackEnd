e556a4709a8279bab43f98e2a182badcf7130d29
DaeHan <daehan128@gmail.com>
2023-10-24 23:13:31 +0900

    refactor : admin code add

--- src/main/java/com/hamtaro/sunflowerplate/controller/AdminController.java
+++ src/main/java/com/hamtaro/sunflowerplate/controller/AdminController.java
@@ -1,14 +1,12 @@
 package com.hamtaro.sunflowerplate.controller;
 
 
+import com.hamtaro.sunflowerplate.dto.ReportDto;
 import com.hamtaro.sunflowerplate.service.AdminService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Log4j2
 @RestController
@@ -20,6 +18,14 @@ public class AdminController {
     @DeleteMapping("/review/delete")
     public ResponseEntity<?> removeAdminReview(@RequestParam Long reviewId){
         return adminService.deleteAdminReview(reviewId);
+    }
+
+    @GetMapping("/review")
+    public ResponseEntity<?> reviewReport(@RequestBody ReportDto reportDto , String memberId){
+
+
+        return adminService.adminReportCheck(reportDto,memberId);
+
     }
 
 }
--- src/main/java/com/hamtaro/sunflowerplate/service/AdminService.java
+++ src/main/java/com/hamtaro/sunflowerplate/service/AdminService.java
@@ -1,21 +1,27 @@
 package com.hamtaro.sunflowerplate.service;
 
+import com.hamtaro.sunflowerplate.dto.ReportDto;
+import com.hamtaro.sunflowerplate.entity.review.ReportEntity;
 import com.hamtaro.sunflowerplate.entity.review.ReviewEntity;
+import com.hamtaro.sunflowerplate.repository.ReportRepository;
+import com.hamtaro.sunflowerplate.repository.RequestRepository;
 import com.hamtaro.sunflowerplate.repository.ReviewRepository;
+import com.hamtaro.sunflowerplate.repository.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Log4j2
 @Service
 @RequiredArgsConstructor
 public class AdminService {
     private final ReviewRepository reviewRepository;
+    private final MemberRepository memberRepository;
+    private final ReportRepository reportRepository;
+    private final RequestRepository requestRepository;
 
     public ResponseEntity<?> deleteAdminReview(Long reviewId){
         Map<String, String> result = new HashMap<>();
@@ -29,5 +35,25 @@ public class AdminService {
             return ResponseEntity.status(403).body(result);
         }
 
+    }
+    //관리자 신고내역 조회
+    public ResponseEntity<?> adminReportCheck(ReportDto reportDto , String memberId) {
+
+
+        List<ReportEntity> reportEntityList = reportRepository.findByMemberEntity_MemberId(Long.valueOf(memberId));
+
+        List<ReportDto> reportDtoList = new ArrayList<>();
+        for (ReportEntity reportEntity : reportEntityList) {
+            ReportDto reportDtos = ReportDto.builder()
+                    .reviewId(reportEntity.getReportId())
+                    .reportCategory(reportEntity.getReportCategory())
+                    .reportAt(reportEntity.getReportAt())
+                    .memberId(reportEntity.getReportId())
+                    .build();
+
+            reportDtoList.add(reportDtos);
+        }
+
+        return ResponseEntity.status(200).body(reportDtoList);
     }
 }
