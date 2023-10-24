c9dcbc73e297605094b884a76443296023fd6010
Taeyeon-0314 <lty0314@gmail.com>
2023-10-24 14:26:43 +0900

    feat : github-actions cicd 구축

--- src/main/java/com/hamtaro/sunflowerplate/SunflowerPlateProjectApplication.java
+++ src/main/java/com/hamtaro/sunflowerplate/SunflowerPlateProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SunflowerPlatProjectApplication {
+public class SunflowerPlateProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SunflowerPlatProjectApplication.class, args);
+		SpringApplication.run(SunflowerPlateProjectApplication.class, args);
 	}
 
 }
