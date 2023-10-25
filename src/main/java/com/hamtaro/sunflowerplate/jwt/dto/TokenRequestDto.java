02bfe350b5149078aa1244453e88aad989bd29d4
Taeyeon-0314 <lty0314@gmail.com>
2023-10-25 16:55:47 +0900

    feat : JWT(액세스토큰/리프레시토큰) 생성 및 액세스토큰 재발급 구현

---
+++ src/main/java/com/hamtaro/sunflowerplate/jwt/dto/TokenRequestDto.java
@@ -0,0 +1,23 @@
+package com.hamtaro.sunflowerplate.jwt.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenRequestDto {
+
+    @Schema(description = "액세스 토큰")
+    private String accessToken;
+    @Schema(description = "액세스 토큰 생성 시간")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+    private Date issuedAt;
+    @Schema(description = "액세스 토큰 만료 시간")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+    private Date accessTokenExpireDate;
+}
