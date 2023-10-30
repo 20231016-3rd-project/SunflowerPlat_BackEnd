4735012148638d0025cfab7cce2d18ddffef5613
chvnni <channi2017@icloud.com>
2023-10-30 20:00:01 +0900

    feat: 마이페이지 리뷰조회, 삭제, 내장소조회 구현(리뷰 수정은 진행중)

---
+++ src/main/java/com/hamtaro/sunflowerplate/dto/member/UpdateReviewDto.java
@@ -0,0 +1,25 @@
+package com.hamtaro.sunflowerplate.dto.member;
+
+import com.hamtaro.sunflowerplate.dto.ReviewImageDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UpdateReviewDto {
+
+    private Long reviewId;
+
+    private String reviewContent;
+
+    private List<MultipartFile> imageFile;
+
+}
--- src/main/java/com/hamtaro/sunflowerplate/repository/ReviewRepository.java
+++ src/main/java/com/hamtaro/sunflowerplate/repository/ReviewRepository.java
@@ -3,9 +3,12 @@ package com.hamtaro.sunflowerplate.repository;
 import com.hamtaro.sunflowerplate.entity.review.ReviewEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
     Optional<ReviewEntity> findByReviewId(Long reviewId);
+
+    List<ReviewEntity> findByMemberEntity_MemberId(Long id);
 }
